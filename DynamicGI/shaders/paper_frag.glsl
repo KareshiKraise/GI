d0cb0f3cc4c3cec03048d44203d35728c84f6ef7
KareshiKraise <vitormoraesaranha@gmail.com>
2021-04-01 13:07:26 -0300

    Removal of old ode

--- DynamicGI/shaders/paper_frag.glsl
+++
@@ -1,7 +0,0 @@
-#version 430
-
-out vec4 color;
-
-void main() {
-	color = vec4(1.0, 0.2, 0.2, 1.0);
-}
--- DynamicGI/shaders/unused/axis_view_frag.glsl
+++
@@ -1,9 +0,0 @@
-#version 430
-
-in vec3 col;
-
-out vec4 fragcolor;
-
-void main() {
-	fragcolor = vec4(col, 1.0);
-}
